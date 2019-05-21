9c8427d402ae69362f98fc8ec93318a07261eb27
marzipansnail <44766019+marzipansnail@users.noreply.github.com>
2019-05-21 17:40:41 +0200

    Update Subscription.java

--- src/main/java/com/task/Subscription.java
+++ src/main/java/com/task/Subscription.java
@@ -2,14 +2,20 @@ package com.task;
 
 import java.util.Date;
 
-public class Subscription {
+class Subscription
+{
     private IGame game;
-    private IDevice device;
+    private Device device;
     private Date lastUpdate;
+    public Subscription (IGame game, Device device) {
+        this.game = game;
+        this.device = device;
+        this.lastUpdate = new Date();
+    }
 
-
-    public void notifyDevice(){
-        if (!isUpToDate()) {
+    public void notifyDevice() {
+        if (!isUpToDate())
+        {
             device.getMessage(game.getLatestResults());
             this.lastUpdate = new Date();
         }
@@ -19,7 +25,7 @@ public class Subscription {
         return game;
     }
 
-    public IDevice getDevice() {
+    public Device getDevice() {
         return device;
     }
 
