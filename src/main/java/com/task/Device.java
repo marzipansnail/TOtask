083313625956f940c9451af8f77606f51904c44d
marzipansnail <44766019+marzipansnail@users.noreply.github.com>
2019-05-21 17:38:28 +0200

    Update and rename IDevice.java to Device.java

---
+++ src/main/java/com/task/Device.java
@@ -0,0 +1,7 @@
+package com.task;
+
+abstract class Device{
+    protected String name;
+    public abstract void getMessage(String msg);
+
+}
--- src/main/java/com/task/IDevice.java
+++
@@ -1,6 +0,0 @@
-package com.task;
-
-public interface IDevice {
-    public void getMessage(String msg);
-    public void chooseGame();
-}
