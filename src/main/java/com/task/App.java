b1d49171fb21fa4ba2568f90822682caf204b62c
marzipansnail <44766019+marzipansnail@users.noreply.github.com>
2019-05-21 17:32:26 +0200

    Update App.java

--- src/main/java/com/task/App.java
+++ src/main/java/com/task/App.java
@@ -3,6 +3,26 @@ package com.task;
 public class App {
 
     public static void main(String args[]){
+        
+        SubscriptionManager subManager = new SubscriptionManager ();
+        Game lotto = new Game("lotto");
+        Game bingo = new Game("bingo");
+        Subscription sub1 = new Subscription(bingo, new IOSDevice("A"));
+        Subscription sub2 = new Subscription(lotto, new AndroidDevice("B"));
+        Subscription sub3 = new Subscription(bingo, new WindowsPhoneDevice("C"));
+        Subscription sub4 = new Subscription(lotto, new BlackberryDevice("D"));
+        Subscription sub5 = new Subscription(bingo, new BlackberryDevice("E"));
+        subManager.register(sub1);
+        subManager.register(sub2);
+        subManager.register(sub3);
+        subManager.register(sub4);
+        subManager.register(sub5);
+        lotto.play();
+        bingo.play();
+        subManager.notifySubscribers();
+        lotto.play();
+        bingo.play();
+        subManager.notifySubscribers();
 
     }
 }
